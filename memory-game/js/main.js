3521f862f05b63a08c16c0559e15c52fff28efdc
Sarah <sarahrosky@gmail.com>
2020-04-18 22:24:16 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,19 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
+const cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+//if (cardsInPlay.length === 2) {console.log("You've played two cards");}
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+
+//const cardsInPlay = cardsInPlay[0] === cardsInPlay[2] ? alert("You found a match!") : alert("Sorry, try again.");
